bcda0b0d298eea4824c93b72d658e7cce42f43b6
EC2 Default User <ec2-user@ip-172-31-60-38.ec2.internal>
2021-02-01 08:33:18 +0000

    課題終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
@@ -27,9 +28,9 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
-      flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(@book.id)
+    if @book.update(book_params)
+       @book.save
+      redirect_to book_path(@book), notice: "Book was successfully updated."
     else
       render :edit
     end
